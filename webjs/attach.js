f54f944d220bbf7f38bebb623f5c8a7b999ff94b
zhmhbest <zhmhbest@gmail.com>
2020-10-22 14:16:37 +0800

    打包方式

--- webjs/attach.js
+++ webjs/attach.js
@@ -2,14 +2,26 @@ const fs = require('fs');
 const [, , ...argv] = process.argv;
 const FilenamePackage = 'package.json';
 const currentPackage = JSON.parse(fs.readFileSync(FilenamePackage).toString());
-const currentScripts = currentPackage['scripts'];
+
 for (let item of argv) {
     let sp = item.search(/=/); if (0===sp) continue;
-    let key = item.substr(0, sp).trim();
     let val = item.substr(sp + 1).trim();
+    let loc = item.substr(0, sp).trim().split('>');
+    let dir = loc.slice(0, -1);
+    let key = loc.slice(-1);
+
+    let cur = currentPackage;
+    if (0!==dir.length) {
+        for (let item of dir) {
+            if (undefined===cur) {
+                console.error("Unexpect key", dir);
+                process.exit(1);
+            } cur = cur[item];
+        }
+    }
     if (0===val.length)
-        delete currentScripts[key];
+        delete cur[key];
     else
-        currentScripts[key] = val;
+        cur[key] = val;
 }
 fs.writeFileSync(FilenamePackage, JSON.stringify(currentPackage, undefined, '    '));
