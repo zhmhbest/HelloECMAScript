293d152b430a1e8aeef737627750e3059a69cde4
zhmhbest <zhmhbest@gmail.com>
2020-11-02 20:15:27 +0800

    update form

--- webjs/src/index.js
+++ webjs/src/index.js
@@ -3,10 +3,10 @@ require("@babel/polyfill");  // 解决Promise
 module.exports = {
     hash: require("./lib/hash.ts"),
     screen: require("./lib/screen.ts"),
+    form: require("./lib/form.ts"),
 
     cookie: require("./lib/cookie"),
     ajax: require("./lib/ajax"),
     file: require("./lib/file"),
-    form: require("./lib/form"),
     date: require("./lib/date"),
 };
