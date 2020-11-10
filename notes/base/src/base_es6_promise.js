b19364a34fdc58003eeabf46b9ce5b91a54372bc
zhmhbest <zhmhbest@gmail.com>
2020-11-10 13:20:13 +0800

    async

--- notes/base/src/base_es6_promise.js
+++ notes/base/src/base_es6_promise.js
@@ -1,25 +1,69 @@
-/*
- * Promise
- */
 
-function doSomething(ok) {
+/**
+ * doPromise
+ * @return {Promise}
+ */
+function doPromise(ok) {
     return new Promise((resolve, reject) => {
         if (ok) {
-            resolve("Success!");
+            resolve("Promise: Success!");
         } else {
-            reject("Error!");
+            reject("Promise: Error!");
         }
     });
 }
 
-doSomething(true).then(res => {
+doPromise(true).then(res => {
     console.log(res)
 }).catch(err => {
     console.log(err)
 });
 
-doSomething(false).then(res => {
+doPromise(false).then(res => {
     console.log(res)
 }).catch(err => {
     console.log(err)
 });
+
+// ■■■■■■■■■■■■■■■■ ■■■■■■■■■■■■■■■■
+
+/**
+ * doasync
+ * @return {Promise}
+ */
+async function doasync(ok) {
+    if (ok) {
+        return "async: Success!";
+    } else {
+        return "async: Error!";
+    }
+}
+
+doasync(true).then(res => {
+    console.log(res)
+});
+
+doasync(false).then(res => {
+    console.log(res)
+});
+
+// ■■■■■■■■■■■■■■■■ ■■■■■■■■■■■■■■■■
+
+/*
+ * async await
+ */
+
+function getPromise(num) {
+    return new Promise((resolve, reject) => {
+        setTimeout(function() {
+            resolve(num);
+        }, 500);
+    });
+}
+
+(async function() {
+    for (let i=0; i<3; i++) {
+        let res = await getPromise(i);
+        console.log(res );
+    }
+})();
