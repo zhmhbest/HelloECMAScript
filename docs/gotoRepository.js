cbbc76e96442722c6a3bc65afd83f4c78a39eb9b
zhmhbest <zhmhbest@gmail.com>
2021-01-16 18:02:40 +0800

    gR

--- docs/gotoRepository.js
+++ docs/gotoRepository.js
@@ -1,14 +1,25 @@
 function gotoRepository(source) {
-    let matcher = window.location.host.match(/.+(?=\.github\.io$)/);
+    const HOST = window.location.host;
+    const PATHNAME = window.location.pathname;
+    // console.log(HOST);
+    let matcher;
+
+    // Github
+    matcher = HOST.match(/.+(?=\.github\.io$)/);
     if (null !== matcher) {
         const pathdir = 'docs';
-        const pathname = window.location.pathname;
         const username = matcher[0];
-        const pos = pathname.indexOf('/', 1);
-        const repository = pathname.substr(1, pos - 1);
-        const sub = pathname.substr(pos, pathname.length - pos);
+        const pos = PATHNAME.indexOf('/', 1);
+        const repository = PATHNAME.substr(1, pos - 1);
+        const sub = PATHNAME.substr(pos, PATHNAME.length - pos);
         const path = sub.match(/.+(?=\/.+?\.html$)/)[0];
         const destination = `https://github.com/${username}/${repository}/tree/master/${pathdir}${path}/${source}`;
         window.location = destination;
+    }
+
+    // File
+    if (null !== HOST.match(/^127.0.0.1/)) {
+        const path = PATHNAME.match(/.+(?=\/.+?\.html$)/)[0];
+        window.location.pathname = `${path}/${source}`;
     }
 }
